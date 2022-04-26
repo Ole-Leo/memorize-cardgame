073328f7757984a95e7ee1170c975ede8d43dc56
Olesya <olesya.leonovich@gmail.com>
2022-04-26 17:27:46 +0300

    add 2 and 3 page

---
+++ cards-list.js
@@ -0,0 +1,182 @@
+window.cards = `[
+    {
+      "name": "6 clubs",
+      "front-img": "img/card-img/6 clubs.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "7 clubs",
+      "front-img": "img/card-img/7 clubs.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "8 clubs",
+      "front-img": "img/card-img/8 clubs.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "9 clubs",
+      "front-img": "img/card-img/9 clubs.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "10 clubs",
+      "front-img": "img/card-img/10 clubs.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "jack clubs",
+      "front-img": "img/card-img/jack clubs.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "queen clubs",
+      "front-img": "img/card-img/queen clubs.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "king clubs",
+      "front-img": "img/card-img/king clubs.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "ace clubs",
+      "front-img": "img/card-img/ace clubs.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "6 diamonds",
+      "front-img": "img/card-img/6 diamonds.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "7 diamonds",
+      "front-img": "img/card-img/7 diamonds.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "8 diamonds",
+      "front-img": "img/card-img/8 diamonds.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "9 diamonds",
+      "front-img": "img/card-img/9 diamonds.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "10 diamonds",
+      "front-img": "img/card-img/10 diamonds.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "jack diamonds",
+      "front-img": "img/card-img/jack diamonds.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "queen diamonds",
+      "front-img": "img/card-img/queen diamonds.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "king diamonds",
+      "front-img": "img/card-img/king diamonds.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "ace diamonds",
+      "front-img": "img/card-img/ace diamonds.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "6 hearts",
+      "front-img": "img/card-img/6 hearts.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "7 hearts",
+      "front-img": "img/card-img/7 hearts.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "8 hearts",
+      "front-img": "img/card-img/8 hearts.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "9 hearts",
+      "front-img": "img/card-img/9 hearts.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "10 hearts",
+      "front-img": "img/card-img/10 hearts.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "jack hearts",
+      "front-img": "img/card-img/jack hearts.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "queen hearts",
+      "front-img": "img/card-img/queen hearts.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "king hearts",
+      "front-img": "img/card-img/king hearts.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "ace hearts",
+      "front-img": "img/card-img/ace hearts.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "6 spades",
+      "front-img": "img/card-img/6 spades.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "7 spades",
+      "front-img": "img/card-img/7 spades.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "8 spades",
+      "front-img": "img/card-img/8 spades.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "9 spades",
+      "front-img": "img/card-img/9 spades.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "10 spades",
+      "front-img": "img/card-img/10 spades.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "jack spades",
+      "front-img": "img/card-img/jack spades.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "queen spades",
+      "front-img": "img/card-img/queen spades.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "king spades",
+      "front-img": "img/card-img/king spades.png",
+      "back-img": "img/back-side-card.png"
+    },
+    {
+      "name": "ace spades",
+      "front-img": "img/card-img/ace spades.png",
+      "back-img": "img/back-side-card.png"
+    }
+  ]`;
